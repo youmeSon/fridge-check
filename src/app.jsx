3dd1f2793bd20bde0b228dc16ddb17f96cbd098b
Youme <rosydarling@naver.com>
2022-05-25 08:56:49 +0100

    bring all state and functions to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import './app.css';
 import FridgeItems from './components/fridgeItems';
 
-function App() {
-  return <FridgeItems/>
+class App extends Component {
+  state = {
+    fridgeItems: [
+        { id:1, name: 'vegetable', count: 0},
+        { id:2, name: 'beer', count: 0},
+        { id:3, name: 'fruits', count: 0},
+    ],
+  };
+
+  handleIncrement = (item) => {
+    const fridgeItems = [...this.state.fridgeItems];
+    const index = fridgeItems.indexOf(item);
+    fridgeItems[index].count++;
+    this.setState({fridgeItems})
+}
+
+handleDecrement = (item) => {
+    const fridgeItems = [...this.state.fridgeItems];
+    const index = fridgeItems.indexOf(item);
+    const count = fridgeItems[index].count - 1;
+    fridgeItems[index].count = count < 0 ? 0 : count;
+    this.setState({fridgeItems})
+}
+
+handleDelete = (item) => {
+    const fridgeItems = this.state.fridgeItems.filter(thing => thing.id !== item.id);
+    this.setState({fridgeItems})
+}
+
+  render() {
+    return(
+      <FridgeItems fridgeItems={this.state.fridgeItems} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete}/>
+    );
+  }
+  
 }
 
 export default App;
+
--- src/components/fridgeItems.jsx
+++ src/components/fridgeItems.jsx
@@ -2,38 +2,13 @@ import React, { Component } from 'react';
 import FridgeItem from './fridgeItem';
 
 class FridgeItems extends Component {
-    state = {
-        fridgeItems: [
-            { id:1, name: 'vegetable', count: 0},
-            { id:2, name: 'beer', count: 0},
-            { id:3, name: 'fruits', count: 0},
-        ],
-    };
 
-    handleIncrement = (item) => {
-        const fridgeItems = [...this.state.fridgeItems];
-        const index = fridgeItems.indexOf(item);
-        fridgeItems[index].count++;
-        this.setState({fridgeItems})
-    }
 
-    handleDecrement = (item) => {
-        const fridgeItems = [...this.state.fridgeItems];
-        const index = fridgeItems.indexOf(item);
-        const count = fridgeItems[index].count - 1;
-        fridgeItems[index].count = count < 0 ? 0 : count;
-        this.setState({fridgeItems})
-    }
-
-    handleDelete = (item) => {
-        const fridgeItems = this.state.fridgeItems.filter(thing => thing.id !== item.id);
-        this.setState({fridgeItems})
-    }
     render() {
         return (
         <ul>
-            {this.state.fridgeItems.map(item => (
-                    <FridgeItem key={item.id} item={item} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete}/>
+            {this.props.fridgeItems.map(item => (
+                    <FridgeItem key={item.id} item={item} onIncrement={this.props.onIncrement} onDecrement={this.props.onDecrement} onDelete={this.props.onDelete}/>
             ))
             }
         </ul>
